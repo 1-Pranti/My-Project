3a4914328e3d70db9baea17e65e394d34fea3613
1-Pranti <101864460+1-Pranti@users.noreply.github.com>
2023-03-16 00:10:06 +0600

    Add files via upload

---
+++ Jarvis_Main.py
@@ -0,0 +1,271 @@
+import pyttsx3
+import speech_recognition 
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import webbrowser
+import os
+import pyautogui
+import random
+from plyer import notification
+from pygame import mixer
+import speedtest
+
+for i in range(3):
+    a = input("Enter the Password Maam to Open Jarvis :- ")
+    pw_file = open("password.txt","r")
+    pw = pw_file.read()
+    pw_file.close()
+    if (a==pw):
+        print("WELCOME MAAM ! PLEASE SPEAK [WAKE UP] TO LOAD ME UP")
+        break
+    elif (i==2 and a!=pw):
+        exit()
+
+    elif (a!=pw):
+        print("Try Again")
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+rate = engine.setProperty("rate",170)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def takeCommand():
+    r = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        print("Listening........")
+        r.pause_threshold = 1
+        r.energy_threshold = 300
+        audio = r.listen(source,0,4)
+
+    try:
+        print("Understanding....")
+        query  = r.recognize_google(audio,language='en-us')
+        print(f"You Said: {query}\n")
+    except Exception as e:
+        print("Say that again")
+        return "None"
+    return query
+
+def alarm(query):
+    timehere = open("Alarmtext.txt","a")
+    timehere.write(query)
+    timehere.close()
+    os.startfile("alarm.py")
+
+if __name__ == "__main__":
+    while True:
+        query = takeCommand().lower()
+        if "wake up" in query:
+            from GreetMe import greetMe
+            greetMe()
+
+            while True:
+                query = takeCommand().lower()
+
+                if "change password" in query:
+                    speak("What is the new password")
+                    new_pw = input("Enter the new password\n")
+                    new_password = open("password.txt","w")
+                    new_password.write(new_pw)
+                    new_password.close()
+                    speak("Done maam")
+                    speak(f"Your new password is{new_pw}")
+                
+                elif "hello" in query:
+                    speak("Hello maam, how are you ?")
+                elif "i am fine" in query:
+                    speak("that's great, maam")
+                elif "how are you" in query:
+                    speak("Perfect, maam")
+                elif "thank you" in query:
+                    speak("you are welcome, maam")
+
+                elif "play a song" in query:
+                    speak("Playing your favourite songs, maam")
+                    a = (1,2)
+                    b = random.choice(a)
+                    if b==1:
+                        webbrowser.open("https://www.youtube.com/watch?v=HpphFd_mzXE")
+                    elif b==2:
+                        webbrowser.open("https://www.youtube.com/watch?v=lJ45aQ0xHeM")
+
+                elif "open" in query:
+                    from Dictapp import openappweb
+                    openappweb(query)
+                elif "close" in query:
+                    from Dictapp import closeappweb
+                    closeappweb(query)
+                    
+                elif "google" in query:
+                    from SearchNow import searchGoogle
+                    searchGoogle(query)
+                elif "youtube" in query:
+                    from SearchNow import searchYoutube
+                    searchYoutube(query)
+                elif "wikipedia" in query:
+                    from SearchNow import searchWikipedia
+                    searchWikipedia(query)
+
+                elif "temperature" in query:
+                    search = "temperature in Dhaka"
+                    url = f"https://www.google.com/search?q={search}"
+                    r  = requests.get(url)
+                    data = BeautifulSoup(r.text,"html.parser")
+                    temp = data.find("div", class_ = "BNeawe").text
+                    speak(f"current{search} is {temp}")
+
+                elif "weather" in query:
+                    search = "weather in Dhaka"
+                    url = f"https://www.google.com/search?q={search}"
+                    r  = requests.get(url)
+                    data = BeautifulSoup(r.text,"html.parser")
+                    temp = data.find("div", class_ = "BNeawe").text
+                    speak(f"current{search} is {temp}")
+
+                elif "the time" in query:
+                    strTime = datetime.datetime.now().strftime("%H:%M")    
+                    speak(f"Maam, the time is {strTime}")
+
+                elif "finally go to sleep" in query:
+                    speak("Going to sleep, maam")
+                    exit()
+
+                elif "set an alarm" in query:
+                    print("input time example:- 10 and 10 and 10")
+                    speak("Set the time")
+                    a = input("Please tell the time :- ")
+                    alarm(a)
+                    speak("Done, maam")
+
+                elif "pause" in query:
+                    pyautogui.press("k")
+                    speak("video paused")
+                elif "play" in query:
+                    pyautogui.press("k")
+                    speak("video played")
+                elif "mute" in query:
+                    pyautogui.press("m")
+                    speak("video muted")
+                elif "volume up" in query:
+                    from keyboard import volumeup
+                    speak("Turning volume up,maam")
+                    volumeup()
+                elif "volume down" in query:
+                    from keyboard import volumedown
+                    speak("Turning volume down, maam")
+                    volumedown()
+
+                elif "remember that" in query:
+                    rememberMessage = query.replace("remember that","")
+                    rememberMessage = query.replace("jarvis","")
+                    speak("You told me "+rememberMessage)
+                    remember = open("Remember.txt","a")
+                    remember.write(rememberMessage)
+                    remember.close()
+                elif "what do you remember" in query:
+                    remember = open("Remember.txt","r")
+                    speak("You told me to remember that" + remember.read())
+        
+                elif "news" in query:
+                    from NewsRead import latestnews
+                    latestnews()
+
+                elif "calculate" in query:
+                    from Calculatenumbers import WolfRamAlpha
+                    from Calculatenumbers import Calc
+                    query = query.replace("calculate","")
+                    query = query.replace("jarvis","")
+                    Calc(query)
+
+                elif "whatsapp" in query:
+                    from Whatsapp import sendMessage
+                    sendMessage()
+
+                elif "shutdown the system" in query:
+                    speak("Are You sure you want to shutdown")
+                    shutdown = input("Do you wish to shutdown your computer? (yes/no)")
+                    if shutdown == "yes":
+                        os.system("shutdown /s /t 1")
+                    elif shutdown == "no":
+                        break
+            
+                elif "schedule my day" in query:
+                    tasks = []
+                    speak("Do you want to clear old tasks (Plz speak YES or NO)")
+                    query = takeCommand().lower()
+                    if "yes" in query:
+                        file = open("tasks.txt","w")
+                        file.write(f"")
+                        file.close()
+                        no_tasks = int(input("Enter the no. of tasks :- "))
+                        i = 0
+                        for i in range(no_tasks):
+                            tasks.append(input("Enter the task :- "))
+                            file = open("tasks.txt","a")
+                            file.write(f"{i}. {tasks[i]}\n")
+                            file.close()
+                    elif "no" in query:
+                        i = 0
+                        no_tasks = int(input("Enter the no. of tasks :- "))
+                        for i in range(no_tasks):
+                            tasks.append(input("Enter the task :- "))
+                            file = open("tasks.txt","a")
+                            file.write(f"{i}. {tasks[i]}\n")
+                            file.close()
+
+                elif "show my schedule" in query:
+                    file = open("tasks.txt","r")
+                    content = file.read()
+                    file.close()
+                    mixer.init()
+                    mixer.music.load("notification.mp3")
+                    mixer.music.play()
+                    notification.notify(
+                    title = "My schedule :-",
+                    message = content,
+                    timeout = 15
+                    )
+
+                elif "open" in query:
+                    query = query.replace("open","")
+                    query = query.replace("jarvis","")
+                    pyautogui.press("super")
+                    pyautogui.typewrite(query)
+                    pyautogui.sleep(2)
+                    pyautogui.press("enter")   
+
+                elif "internet speed" in query:
+                    wifi  = speedtest.Speedtest()
+                    upload_net = wifi.upload()/1048576         
+                    download_net = wifi.download()/1048576
+                    print("Wifi Upload Speed is", upload_net)
+                    print("Wifi download speed is ",download_net)
+                    speak(f"Wifi download speed is {download_net}")
+                    speak(f"Wifi Upload speed is {upload_net}")
+                elif "play a game" in query:
+                    from game import game_play
+                    game_play()
+
+                elif "screenshot" in query:
+                     import pyautogui
+                     im = pyautogui.screenshot()
+                     im.save("ss.jpg")
+                
+                elif "click my photo" in query:
+                    pyautogui.press("super")
+                    pyautogui.typewrite("camera")
+                    pyautogui.press("enter")
+                    pyautogui.sleep(2)
+                    speak("SMILE")
+                    pyautogui.press("enter")
+                
+                elif "translate" in query:
+                    from Translator import translategl
+                    query = query.replace("jarvis","")
+                    query = query.replace("translate","")
+                    translategl(query)
